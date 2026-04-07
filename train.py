fabcf4e80dbd850552bcc5c1300b201ef0c32fcc
Envoutante <eenvoutante@gmail.com>
2026-04-08 02:19:45 +0800

    experiment: device batch 4

--- train.py
+++ train.py
@@ -31,6 +31,7 @@ TIME_BUDGET = 1200  # 20min override
 TIME_BUDGET = 1200  # 20min override
 TIME_BUDGET = 1200  # 20min override
 TIME_BUDGET = 1200  # 20min override
+TIME_BUDGET = 1200  # 20min override
 
 # ---------------------------------------------------------------------------
 # GPT Model
@@ -455,7 +456,7 @@ FINAL_LR_FRAC = 0.0     # final LR as fraction of initial
 
 # Model size
 DEPTH = 8               # number of transformer layers
-DEVICE_BATCH_SIZE = 8# per-device batch size (reduce if OOM)
+DEVICE_BATCH_SIZE = 4# per-device batch size (reduce if OOM)
 
 # ---------------------------------------------------------------------------
 # Setup: tokenizer, model, optimizer, dataloader
