81828460120bb607abeddf883f88a36f9628654d
Envoutante <eenvoutante@gmail.com>
2026-04-08 09:30:02 +0800

    experiment: total 2**17 emb lr 0.52734

--- train.py
+++ train.py
@@ -37,6 +37,7 @@ TIME_BUDGET = 1200  # 20min override
 TIME_BUDGET = 1200  # 20min override
 TIME_BUDGET = 1200  # 20min override
 TIME_BUDGET = 1200  # 20min override
+TIME_BUDGET = 1200  # 20min override
 
 # ---------------------------------------------------------------------------
 # GPT Model
@@ -449,7 +450,7 @@ WINDOW_PATTERN = "SSSL" # sliding window pattern: L=full, S=half context
 
 # Optimization
 TOTAL_BATCH_SIZE = 2**17# ~524K tokens per optimizer step
-EMBEDDING_LR = 0.70312# learning rate for token embeddings (Adam)
+EMBEDDING_LR = 0.52734# learning rate for token embeddings (Adam)
 UNEMBEDDING_LR = 0.004  # learning rate for lm_head (Adam)
 MATRIX_LR = 0.01582# learning rate for matrix parameters (Muon)
 SCALAR_LR = 0.5         # learning rate for per-layer scalars (Adam)
