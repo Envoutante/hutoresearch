1b9ba8ba194db51613c0a89595d80bf27a037d9e
Envoutante <eenvoutante@gmail.com>
2026-04-15 11:39:28 +0800

    candidate: emb lr 0.20 from best 0.22524

--- train.py
+++ train.py
@@ -567,7 +567,7 @@ WINDOW_PATTERN = "SSSL"  # sliding window pattern: L=full, S=half context
 
 # Optimization
 TOTAL_BATCH_SIZE = 2**17  # ~524K tokens per optimizer step
-EMBEDDING_LR = 0.23  # learning rate for token embeddings (Adam)
+EMBEDDING_LR = 0.20  # learning rate for token embeddings (Adam)
 UNEMBEDDING_LR = 0.004  # learning rate for lm_head (Adam)
 MATRIX_LR = 0.01582  # learning rate for matrix parameters (Muon)
 SCALAR_LR = 0.5  # learning rate for per-layer scalars (Adam)
