767973392fb950856d88f78063f74fc545fac813
Envoutante <eenvoutante@gmail.com>
2026-04-16 20:19:13 +0800

    candidate: iteration 1

--- train.py
+++ train.py
@@ -450,6 +450,7 @@ FINAL_LR_FRAC = 0.0     # final LR as fraction of initial
 # Model size
 DEPTH = 8               # number of transformer layers
 DEVICE_BATCH_SIZE = 4# per-device batch size (reduce if OOM)
+MATRIX_LR = 0.01898# learning rate for matrix parameters (Muon)
 
 # ---------------------------------------------------------------------------
 # Setup: tokenizer, model, optimizer, dataloader
